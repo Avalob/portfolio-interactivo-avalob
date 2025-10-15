f0c1d88fde4fe5cb31e43b0221207e0bab78248c
Andrea <andreavallob22@gmail.com>
2025-10-15 03:39:55 +0200

    Updates

---
+++ assets/HelpModal-B_uVkbjH.js
@@ -0,0 +1,1 @@
+import{j as e}from"./framer-motion-C07CzjY3.js";import"./react-vendor-BG0u1ZPm.js";import{B as o}from"./index-Boj4EawL.js";import{H as t}from"./HelpGuideContent-D7MRsdYw.js";function i({isOpen:i,onClose:r}){return e.jsx(o,{isOpen:i,onClose:r,title:"Guía de Ayuda",icon:"🎮",maxWidth:"90%",maxHeight:"90%",children:e.jsx(t,{})})}export{i as default};
